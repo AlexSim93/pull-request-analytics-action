e846bf7efe9942f4f6cbf7704333eb6942749c02
Aleksei Simatov <alexsim1993@gmail.com>
2023-12-09 12:17:12 +0400

    Feature: handling rate limit errors

--- src/controllers/octokit.ts
+++ src/controllers/octokit.ts
@@ -1,6 +1,28 @@
 import { Octokit } from "octokit";
 import * as core from "@actions/core";
+import { throttling } from "@octokit/plugin-throttling";
+
+Octokit.plugin(throttling);
 
 export const octokit = new Octokit({
   auth: core.getInput("GITHUB_TOKEN") || process.env.GITHUB_TOKEN,
+  throttle: {
+    onSecondaryRateLimit: (_, options) => {
+      octokit.log.error(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+      );
+      core.setFailed(
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+      );
+    },
+    onRateLimit: (_, options) => {
+      octokit.log.error(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      );
+      core.setFailed(
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      );
+    },
+    enabled: true,
+  },
 });
